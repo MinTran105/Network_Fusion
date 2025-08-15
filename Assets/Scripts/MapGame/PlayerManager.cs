29f73ac2e0f13d6e821c8b711ee3666118043129
MinTran105 <silversea105@gmail.com>
2025-08-15 21:47:34 +0700

    update player move

--- Assets/Scripts/MapGame/PlayerManager.cs
+++ Assets/Scripts/MapGame/PlayerManager.cs
@@ -1,38 +1,48 @@
 ﻿using Fusion;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : NetworkBehaviour
 {
-    public NavMeshAgent agent;
+    private CharacterController _characterController;
     private Transform target;
-    [Networked] public float speed { get; set; } = 5f;
+    [SerializeField] private InputActionReference moveAction;
+    public float speed { get; set; } = 55f;
     [Networked] public Vector3 NetworkedDestination { get; set; }
 
     public override void Spawned()
     {
         target = GameObject.FindWithTag("Key").transform;
-        agent = GetComponent<NavMeshAgent>();
+        _characterController = GetComponent<CharacterController>();
     }
 
     public override void FixedUpdateNetwork()
     {
-        // Chỉ host/server tính đường
         if (!Object.HasStateAuthority)
             return;
-        if (!GameManager.Instance.isStarted)
-            return;
+        //if (!GameManager.Instance.isStarted)
+        //    return;
         // Cập nhật đích target di chuyển
-        Vector3 dest = target.position;
-        agent.speed = speed + Random.Range(0,2f); // Tăng tốc độ ngẫu nhiên
-        if (NetworkedDestination != dest)
-        {
-            NetworkedDestination = dest;
-            agent.SetDestination(dest );
-        }
+        Vector3 dir = GetDirection() * speed * Runner.DeltaTime;
+        _characterController.Move(dir);
+        //Vector3 dest = target.position;
+        //agent.speed = speed + Random.Range(0,2f); // Tăng tốc độ ngẫu nhiên
+        //if (NetworkedDestination != dest)
+        //{
+        //    NetworkedDestination = dest;
+        //    agent.SetDestination(dest );
+        //}
 
-        // Gửi vị trí agent về client
-        transform.position = agent.transform.position;
-        transform.rotation = agent.transform.rotation;
+        //// Gửi vị trí agent về client
+        //transform.position = agent.transform.position;
+        //transform.rotation = agent.transform.rotation;
+    }
+
+    private Vector3 GetDirection()
+    {
+        Vector2 input = moveAction.action.ReadValue<Vector2>();
+        Vector3 direction = new Vector3(input.x, 0, input.y);
+        return direction.normalized;
     }
 }
